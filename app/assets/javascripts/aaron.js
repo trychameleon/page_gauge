dc2bcd58d854f9bba5003d27e9cca98a02b1bbdf
Aaron Cody <aaron.cody@gmail.com>
2015-11-12 21:30:14 -0800

    Add base menu size test

--- app/assets/javascripts/aaron.js
+++ app/assets/javascripts/aaron.js
@@ -1,52 +1,57 @@
 window.pagegauge.addGauge(function contentQuantity(site) {
   var bodyNoScript = /\<body([\s\S]*?)\<\/body\>/.exec(site.body)[0].replace(/\<script([\s\S]*?)\<\/script\>/g, '').replace(/\son(.*?)\"([\s\S]*?)\"/g, ''),
-    wordcount = $(bodyNoScript).text().split(' ').length,
-    score = _.min([1, (_.max([0, wordcount - 3000])/ 10000)]);
+    wordcount = $(bodyNoScript).text().replace(/\s+/g, " ").split(' ').length,
+    score = 1 - _.min([1, (_.max([0, wordcount - 500])/ 1000)]);
   return Promise.resolve(score);
 });
 
 window.pagegauge.addGauge(function (site) {
-  startScore = 0.2,
-  ieTagsPresent = /[\s]*\[if[\s]*IE/g.exec(site.body);
+  var startScore = 0.2,
+      ieTagsPresent = /[\s]*\[if[\s]*IE/g.exec(site.body);
 
-  return new Promise(function(resolve) {
-
-    var matches = /<head>([\s\S]*)<\/head>/.exec(site.body);
-
-    if(!matches || !matches.length)
-      return resolve('No head content found');
-
-    var allStyles = '', sheets = [];
-
-    $.each($(matches[1]), function() {
-      var $this = $(this);
-
-      if(this.tagName === 'LINK' && $this.attr('rel') === 'stylesheet') {
-        var url =  pagegauge.util.buildUrl(site, $this.attr('href'));
-
-        sheets.push(pagegauge.createSite(url));
-      }
-    });
-
-    Promise.all(sheets).then(function(datas) {
+  return new Promise(function browserCompatability(resolve) {
+    pagegauge.util.fetchAllStyles(site, function(styles) {
       var prefixes = ["linear-gradient", "box-shadow", "border-radius"];
-      for(var i=0; i< datas.length; i++) {
-        allStyles += datas[i].site.body;
-        allStyles += "\n\n";
-      }
-
-      prefixes = prefixes.map(function(value, index){
+      prefixes = prefixes.map(function(value, index) {
         return {
-          'webkit': new RegExp(value,"g").test(allStyles) == new RegExp('-webkit-' + value,"g").test(allStyles),
-          'moz': new RegExp(value,"g").test(allStyles) == new RegExp('-webkit-' + value,"g").test(allStyles)
+          'webkit': new RegExp(value, "g").test(styles) == new RegExp('-webkit-' + value, "g").test(styles),
+          'moz': new RegExp(value, "g").test(styles) == new RegExp('-webkit-' + value, "g").test(styles)
         };
       });
 
       var webKitStyles = _.every(prefixes, 'moz', true) ? 0.5 : 0,
-          mozStyles = _.every(prefixes, 'moz', true) ? 0.13 : 0;
-
+        mozStyles = _.every(prefixes, 'moz', true) ? 0.13 : 0;
       resolve(startScore + ieTagsPresent + webKitStyles + mozStyles);
     });
   });
 });
 
+
+window.pagegauge.addGauge(function baseMenuSize(site) {
+  var bodyNoScript = /\<body([\s\S]*?)\<\/body\>/.exec(site.body)[0].replace(/\<script([\s\S]*?)\<\/script\>/g, '').replace(/\son(.*?)\"([\s\S]*?)\"/g, ''),
+      possibleNavSelectors = ['nav', 'menu'],
+      possibleNavs = [],
+      proudestParentMenu;
+
+    $(bodyNoScript).find('*').filter(function(){
+      var possibleNav = false;
+      for(var i = 0; i < possibleNavSelectors.length; i++ ){
+        if(this.id.match(possibleNavSelectors[i]) || this.className.match(possibleNavSelectors[i])){
+          possibleNav = true;
+        }
+      }
+      if(possibleNav && possibleNavs.indexOf(this) < 0 && this.children.length > 1){
+        possibleNavs.push(this);
+        return true;
+      }
+    });
+
+    //find shallowest descendant with most amount of children
+    for(var i = 0; i < possibleNavs.length; i++ ){
+      if(!proudestParentMenu || (possibleNavs[i].children.length > proudestParentMenu.children.length && $(proudestParentMenu).not(possibleNavs).length < 1)){
+        proudestParentMenu = possibleNavs[i];
+      }
+    };
+
+    return Promise.resolve(proudestParentMenu.children.length > 7 ? 0 : 1);
+});
