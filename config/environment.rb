552c1727847dedad7550c84c4a7c3496a96d35a1
Brian Norton <brian.nort@gmail.com>
2015-12-08 10:27:36 -0800

    Skip certain escaped characters when generating JSON.

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,14 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 Rails.application.routes.default_url_options[:host] = ENV['APP_URL']
 
-ActiveSupport::JSON::Encoding.escape_html_entities_in_json = false
+chars = ActiveSupport::JSON::Encoding::JSONGemEncoder.const_get(:ESCAPED_CHARS) # ouch
+
+##
+# Originally saw `JSON::GeneratorError·source sequence is illegal/malformed utf-8`
+#   The source of the problem was html site body that we send via JSON
+#
+#   The fix is to not escape those characters, which can be done in a few ways (this is one).
+#
+chars.delete('<')
+chars.delete('>')
+chars.delete('&')
